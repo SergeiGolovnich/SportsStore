c047292bf9f99de8d2f09bd4ad3b1230c71fbb8b
Sergei <sergei@SERGEI-LAPTOP>
2019-11-17 18:45:37 +0300

    Добавлены файлы Layout и ViewStart

--- SportsStore/Startup.cs
+++ SportsStore/Startup.cs
@@ -6,7 +6,7 @@ using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.DependencyInjection;
-using SportsStore.Models
+using SportsStore.Models;
 
 namespace SportsStore
 {
