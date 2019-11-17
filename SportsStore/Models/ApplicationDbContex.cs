d6b39b9d7d5a7587145e35dc0ad91bde0759cff5
Sergei <sergei@SERGEI-LAPTOP>
2019-11-17 19:00:42 +0300

    Создание контекста базы данных

---
+++ SportsStore/Models/ApplicationDbContex.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+
+namespace SportsStore.Models
+{
+    public class ApplicationDbContex:DbContext
+    {
+        public ApplicationDbContex(DbContextOptions<ApplicationDbContex> options): base(options) { }
+        public DbSet<Product> Products { get; set; }
+    }
+}
