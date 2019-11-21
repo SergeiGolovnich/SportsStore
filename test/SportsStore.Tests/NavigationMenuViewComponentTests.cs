708d631bcc282914f78a6930fef4b708c3ad6be8
Sergei <sergei@SERGEI-LAPTOP>
2019-11-21 13:15:28 +0300

    Добавлена подцветка выбранной категории

--- test/SportsStore.Tests/NavigationMenuViewComponentTests.cs
+++ test/SportsStore.Tests/NavigationMenuViewComponentTests.cs
@@ -1,6 +1,8 @@
 ﻿using System.Collections.Generic;
 using System.Linq;
 using Microsoft.AspNetCore.Mvc.ViewComponents;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.AspNetCore.Routing;
 using Moq;
 using SportsStore.Components;
 using SportsStore.Models;
@@ -23,13 +25,41 @@ namespace SportsStore.Tests
                     new Product{ProductId = 3, Name = "P3", Category = "Plums" },
                     new Product{ProductId = 4, Name = "P4", Category = "Oranges" }
                 });
-            NavigationМenuViewComponent target = new NavigationМenuViewComponent(mock.Object);
+            NavigationMenuViewComponent target = new NavigationMenuViewComponent(mock.Object);
 
             //Act
             string[] results = ((IEnumerable<string>)(target.Invoke() as ViewViewComponentResult).ViewData.Model).ToArray();
 
             //Assert
             Assert.True(Enumerable.SequenceEqual(new string[] { "Apples", "Oranges", "Plums" }, results));
+        }
+        [Fact]
+        public void Indicates_Selected_Category()
+        {
+            //Arrage
+            string categoryToSelect = "Apples";
+            Mock<IProductRepository> mock = new Mock<IProductRepository>();
+            mock.Setup(m => m.Products).Returns(
+                new Product[]
+                {
+                    new Product{ProductId = 1, Name = "P1", Category = "Apples" },
+                    new Product{ProductId = 4, Name = "P2", Category = "Oranges" }
+                });
+            NavigationMenuViewComponent target = new NavigationMenuViewComponent(mock.Object);
+            target.ViewComponentContext = new ViewComponentContext
+            {
+                ViewContext = new ViewContext
+                {
+                    RouteData = new RouteData()
+                }
+            };
+            target.RouteData.Values["category"] = categoryToSelect;
+
+            //Act
+            string result = (string)(target.Invoke() as ViewViewComponentResult).ViewData["SelectedCategory"];
+
+            //Assert
+            Assert.Equal(categoryToSelect, result);
         }
     }
 }
