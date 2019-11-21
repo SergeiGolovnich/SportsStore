9de273966b056a9070780accf61136adeaabd82d
Sergei <sergei@SERGEI-LAPTOP>
2019-11-21 19:40:55 +0300

    Добавлен расширяющий метод, возвращающий текущий адрес страницы

---
+++ SportsStore/Infrastructure/UrlExtentions.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Http;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SportsStore.Infrastructure
+{
+    public static class UrlExtentions
+    {
+        public static string PathAndQuery(this HttpRequest httpRequest) =>
+            httpRequest.QueryString.HasValue ?
+            $"{httpRequest.Path}{httpRequest.QueryString}" :
+            httpRequest.Path.ToString();
+    }
+}
