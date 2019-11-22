0a4d84f695a8a0ffc8b2c7599329815a04ff7b50
Sergei <sergei@SERGEI-LAPTOP>
2019-11-22 13:26:39 +0300

    Добавлен класс модели SessionCart

--- SportsStore/Controllers/CartController.cs
+++ SportsStore/Controllers/CartController.cs
@@ -12,16 +12,18 @@ namespace SportsStore.Controllers
     public class CartController : Controller
     {
         private IProductRepository repository;
-        public CartController(IProductRepository repo)
+        private Cart cart;
+        public CartController(IProductRepository repo, Cart cartService)
         {
             repository = repo;
+            cart = cartService;
         }
         public ViewResult Index(string returnUrl)
         {
             return View(
                 new CartIndexViewModel
                 {
-                    Cart = GetCart(),
+                    Cart = cart,
                     ReturnUrl = returnUrl
                 });
         }
@@ -30,9 +32,7 @@ namespace SportsStore.Controllers
             Product product = repository.Products.FirstOrDefault(p => p.ProductId == productId);
             if(product != null)
             {
-                Cart cart = GetCart();
                 cart.AddItem(product, 1);
-                SaveCart(cart);
             }
             return RedirectToAction("Index", new { returnUrl });
         }
@@ -41,20 +41,9 @@ namespace SportsStore.Controllers
             Product product = repository.Products.FirstOrDefault(p => p.ProductId == productId);
             if (product != null)
             {
-                Cart cart = GetCart();
                 cart.RemoveLine(product);
-                SaveCart(cart);
             }
             return RedirectToAction("Index", new { returnUrl });
-        }
-        private Cart GetCart()
-        {
-            Cart cart = HttpContext.Session.GetJson<Cart>("Cart") ?? new Cart();
-            return cart;
-        }
-        private void SaveCart(Cart cart)
-        {
-            HttpContext.Session.SetJson("Cart", cart);
         }
     }
 }
