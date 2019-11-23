48fa3316fe633df248486b2d180a0eb074b0c8c7
Sergei <sergei@SERGEI-LAPTOP>
2019-11-23 20:35:54 +0300

    Добавлена страница просмотра всех товаров для администратора

---
+++ SportsStore/Controllers/AdminController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using SportsStore.Models;
+using SportsStore.Models.ViewModels;
+using SportsStore.Infrastructure;
+
+namespace SportsStore.Controllers
+{
+    public class AdminController : Controller
+    {
+        private IProductRepository repository;
+        public AdminController(IProductRepository repo)
+        {
+            repository = repo;
+        }
+        public ViewResult Index() => View(repository.Products);
+    }
+}
