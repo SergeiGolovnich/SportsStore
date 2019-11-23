f86f4360916bf9699124342d488552e54fdb418e
Sergei <sergei@SERGEI-LAPTOP>
2019-11-23 20:11:15 +0300

    Добавлена страница со списком товаров, ожидающих доставки

--- SportsStore/Controllers/OrderController.cs
+++ SportsStore/Controllers/OrderController.cs
@@ -16,6 +16,19 @@ namespace SportsStore.Controllers
             repository = repo;
             cart = cartService;
         }
+        public ViewResult List() =>
+            View(repository.Orders.Where(o => !o.Shipped));
+        [HttpPost]
+        public IActionResult MarkShipped(int orderId)
+        {
+            Order order = repository.Orders.FirstOrDefault(o => o.OrderId == orderId);
+            if(order != null)
+            {
+                order.Shipped = true;
+                repository.SaveOrder(order);
+            }
+            return RedirectToAction(nameof(List));
+        }
         public ViewResult Checkout() =>
             View(new Order());
         [HttpPost]
